aea6ad3a15b63c0a505988311923750517e72887
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 20:25:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,11 +11,11 @@ director_index = 0
 
 while director_index < nds.length do
   director_name = nds[director_index][:name]
-  totals[director_name]
+  totals[director_name] = 0
   movie_index = 0
 
 while movie_index < nds[director_index][:movies].length do
-  totals 
+  totals[director_name] += nds[]
 
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
