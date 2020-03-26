0bbc93ae8fa7db5b3fa63cc8c42904f720b4bc35
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 19:34:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,28 @@ def directors_totals(nds)
   require 'pp'
   pp directors_database
   
-  
+  row_index = 0
+while row_index < vm.length do
+  puts "Row #{row_index} has #{vm[row_index]} columns"
+ 
+  column_index = 0
+  while column_index < vm[row_index].length do
+    coord = "#{row_index}, #{column_index}"
+    inner_len = vm[row_index][column_index].length
+    # Remember \t is a TAB character for indentation
+    puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+ 
+    inner_index = 0
+    while inner_index < inner_len do
+      puts "\t\t (#{coord}, #{inner_len}) is: #{vm[row_index][column_index][inner_index]}"
+      inner_index += 1
+    end
+ 
+    column_index += 1
+  end
+ 
+  row_index += 1
+end
 
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
