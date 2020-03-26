259cdd9d6f7fca742c043b553a55de9054f55328
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 20:21:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,9 +6,10 @@ def directors_totals(nds)
   # that you know what you're starting with!
   #
   
-totals 
+totals = {}
 director_index = 0 
 
+while director_index
 
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
