3bb2c85599db657906b28597d2b7d6a3ddf00f0e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 19:36:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,19 +9,19 @@ def directors_totals(nds)
   pp directors_database
   
   row_index = 0
-while row_index < vm.length do
-  puts "Row #{row_index} has #{vm[row_index]} columns"
+while row_index < directors_database.length do
+  puts "Row #{row_index} has #{directors_database[row_index]} columns"
  
   column_index = 0
-  while column_index < vm[row_index].length do
+  while column_index < directors_database[row_index].length do
     coord = "#{row_index}, #{column_index}"
-    inner_len = vm[row_index][column_index].length
+    inner_len = directors_database[row_index][column_index].length
     # Remember \t is a TAB character for indentation
-    puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+    puts "\tCoordinate [#{coord}] points to an #{directors_database[row_index][column_index].class} of length #{inner_len}"
  
     inner_index = 0
     while inner_index < inner_len do
-      puts "\t\t (#{coord}, #{inner_len}) is: #{vm[row_index][column_index][inner_index]}"
+      puts "\t\t (#{coord}, #{inner_len}) is: #{directors_database[row_index][column_index][inner_index]}"
       inner_index += 1
     end
  
